1983fc33b337c26f175782c16ff20c58aa103805
ddewantaraq <muhammad.dimas.dewantara@gmail.com>
2025-01-12 10:54:13 +0700

    change var name

--- src/app/api/s3/route.ts
+++ src/app/api/s3/route.ts
@@ -3,10 +3,10 @@ import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
 import { NextResponse } from 'next/server';
 
 const s3 = new S3Client({
-  region: process.env.AWS_REGION!,
+  region: process.env.REGION!,
   credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+    accessKeyId: process.env.ACCESS_KEY_ID!,
+    secretAccessKey: process.env.SECRET_ACCESS_KEY!,
   },
 });
 
@@ -23,7 +23,7 @@ export async function GET(request: Request) {
     }
 
     const command = new GetObjectCommand({
-      Bucket: process.env.AWS_S3_BUCKET,
+      Bucket: process.env.S3_BUCKET,
       Key: key,
     });
 
